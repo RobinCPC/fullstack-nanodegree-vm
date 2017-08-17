a54a69d44b998898c04a1f8a5467f8bf2921d992
robincpc <chienpinchen@gmail.com>
2017-08-17 15:44:52 -0700

    Imporvement: remove unnecessary tables and move VIEW to sql file.

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 #
 # tournament.py -- implementation of a Swiss-system tournament
-# TODO: remove unnecessary TABLE (wins, lose)
 
 import psycopg2
 
@@ -15,8 +14,6 @@ def deleteMatches():
     """Remove all the match records from the database."""
     db = connect()
     c = db.cursor()
-    c.execute("DELETE FROM lose")
-    c.execute("DELETE FROM wins")
     c.execute( "DELETE FROM matches")
     db.commit()
     db.close()
@@ -36,10 +33,9 @@ def countPlayers():
     db = connect()
     c = db.cursor()
     c.execute("SELECT count(*) from players as cnt")
-    cnt = c.fetchall()
-    #print cnt[0]
+    cnt = c.fetchone()
     db.close()
-    return int(cnt[0][0])
+    return int(cnt[0])
 
 
 def registerPlayer(name):
@@ -53,8 +49,8 @@ def registerPlayer(name):
     """
     db = connect()
     c = db.cursor()
-    #query = "INSERT INTO players VALUE (%s)", (name,)
-    c.execute("INSERT INTO players VALUES (%s)", (name,))
+    query = "INSERT INTO players (name) VALUES (%s)"
+    c.execute(query, (name,))
     db.commit()
     db.close()
 
@@ -74,13 +70,18 @@ def playerStandings():
     """
     db = connect()
     c = db.cursor()
-    # create a view to cumulate total matches
-    c.execute("CREATE VIEW matcnt as SELECT player1 as id from matches UNION ALL SELECT player2 from matches")
-    query = "SELECT players.id, players.name, count(wins.winner) as win, count(matcnt.id) as mat from players left join wins on players.id = wins.winner left join matcnt on players.id = matcnt.id group by players.id order by win"
+    query = """
+            SELECT players.id,
+                   players.name,
+                   (SELECT count(matches.winner) from matches
+                    where matches.winner = players.id) as nbr_of_win,
+                   (SELECT count(matcnt.id) from matcnt
+                    where matcnt.id = players.id) as nbr_of_match
+            FROM players group by players.id order by nbr_of_win
+            """
     c.execute(query)
     stand = c.fetchall()
-    #print stand
-    c.execute("DROP VIEW matcnt")
+    #print "STAND", stand
     db.close()
     return stand
 
@@ -94,9 +95,8 @@ def reportMatch(winner, loser):
     """
     db = connect()
     c = db.cursor()
-    c.execute("INSERT INTO wins VALUES (%s)", (winner,) )
-    c.execute("INSERT INTO lose VALUES (%s)", (loser,) )
-    c.execute("INSERT INTO matches VALUES (%s, %s)", (winner, loser,))
+    query = "INSERT INTO matches (winner, loser) VALUES (%s, %s)"
+    c.execute(query, (winner, loser,))
     db.commit()
     db.close()
 
@@ -118,17 +118,20 @@ def swissPairings():
     """
     db = connect()
     c = db.cursor()
-    # create a view to sort by wins
-    c.execute("CREATE VIEW tmp as SELECT players.id, players.name, count(wins.winner) as win from players left join wins on players.id = wins.winner group by players.id order by win DESC")
-    c.execute("CREATE VIEW ranks as SELECT ROW_NUMBER() OVER (order by win) as rank,  * from tmp")
-    c.execute("SELECT * from ranks")
-    tmp = c.fetchall()
+    #c.execute("SELECT * from ranks")
+    #tmp = c.fetchall()
     #print "RANK: ", tmp
-    c.execute("SELECT r1.id, r1.name, r2.id, r2.name FROM ranks r1 JOIN ranks r2 on (r1.rank % 2 = 1) and (r2.rank % 2 = 0) and r1.rank = (r2.rank - 1)")
+    query = """
+            SELECT r1.id,
+                   r1.name,
+                   r2.id,
+                   r2.name
+            FROM ranks r1 JOIN ranks r2 on
+            (r1.rank % 2 = 1) and (r2.rank % 2 = 0) and r1.rank = (r2.rank - 1)
+            """
+    c.execute(query)
     pairs = c.fetchall()
     #print "PAIRS: ", pairs
-    c.execute("DROP VIEW ranks")
-    c.execute("DROP VIEW tmp")
     return pairs
 
 
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -5,14 +5,37 @@
 --
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
+
+-- Check if database is created
+Drop DATABASE IF EXISTS tournament;
+CREATE DATABASE tournament;
+\c tournament
+
+-- Create to TABLE to save basic information of the tournament.
 CREATE TABLE players ( name TEXT,
                        id SERIAL primary key );
 
-CREATE TABLE matches ( player1 SERIAL references players (id),
-                       player2 SERIAL references players (id),
-                       stage INTEGER );
+CREATE TABLE matches ( winner SERIAL references players (id),
+                       loser SERIAL references players (id),
+                       match_id SERIAL primary key );
 
-CREATE TABLE wins ( winner SERIAL references players (id));
+-- Create a matcnt VIEW to merge winner and loser into the same column
+CREATE VIEW matcnt as
+    SELECT winner as id from matches UNION ALL
+    SELECT loser from matches;
 
-CREATE TABLE lose ( loser SERIAL references players (id));
+
+-- create two VIEW to sort by wins
+-- Create a VIEW temp to count how many time each player wins.
+CREATE VIEW tmp as
+    SELECT players.id,
+           players.name,
+           count(matches.winner) as win
+    from players
+    left join matches on players.id = matches.winner
+    group by players.id order by win DESC;
+
+-- Create a VIEW rank to sort player by the times of win.
+CREATE VIEW ranks as
+    SELECT ROW_NUMBER() OVER (order by win) as rank,  * from tmp;
 
