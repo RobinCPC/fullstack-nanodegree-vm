94796907e979e4c41e6021b851dae8b738b65400
robincpc <chienpinchen@gmail.com>
2017-09-24 14:42:16 -0700

    Add templates and forms for final project.

--- vagrant/introCRUD/finalProject.py
+++ vagrant/introCRUD/finalProject.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 
@@ -6,44 +6,67 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/restaurants/')
 def showRestaurants():
-   return "This page will show all restaurants."
+    return render_template("restaurants.html", restaurants = restaurants)
+   #return "This page will show all restaurants."
 
 
-@app.route('/restaurant/new/')
+@app.route('/restaurant/new/', methods=['GET', 'POST'])
 def newRestaurant():
-    return "This page will be for making a new restaurant"
+    return render_template("newRestaurant.html")
+    #return "This page will be for making a new restaurant"
 
 
-@app.route('/restaurant/<int:restaurant_id>/edit/')
+@app.route('/restaurant/<int:restaurant_id>/edit/', methods=['GET', 'POST'])
 def editRestaurant(restaurant_id):
-    return "This page will be for editing restaurant %s" % restaurant_id
+    return render_template("editRestaurant.html", restaurant_id=restaurant_id)
+    #return "This page will be for editing restaurant %s" % restaurant_id
 
 
-@app.route('/restaurant/<int:restaurant_id>/delete/')
+@app.route('/restaurant/<int:restaurant_id>/delete/', methods=['GET', 'POST'])
 def deleteRestaurant(restaurant_id):
-    return "This page will be for deleting restaurant %s" % restaurant_id
+    return render_template("deleteRestaurant.html", restaurant_id=restaurant_id)
+    #return "This page will be for deleting restaurant %s" % restaurant_id
 
 
 @app.route('/restaurant/<int:restaurant_id>/')
 @app.route('/restaurant/<int:restaurant_id>/menu/')
 def showMenu(restaurant_id):
-    return "This page is the menu for restaurant %s" % restaurant_id
+    return render_template("menu.html", restaurant=restaurant, items=items)
+    #return "This page is the menu for restaurant %s" % restaurant_id
 
 
-@app.route('/restaurant/<int:restaurant_id>/menu/new/')
+@app.route('/restaurant/<int:restaurant_id>/menu/new/', methods=['GET', 'POST'])
 def newMenuItem(restaurant_id):
-    return "This page is for making a new menu item for restaurant %s" % restaurant_id
+    return render_template('newMenuItem.html', restaurant_id = restaurant_id)
+    #return "This page is for making a new menu item for restaurant %s" % restaurant_id
 
 
-@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit/')
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit/', methods=['GET', 'POST'])
 def editMenuItem(restaurant_id, menu_id):
-    return "This page is for editing menu item %s" % menu_id
+    return render_template('editMenuItem.html', restaurant_id = restaurant_id, menu_id=menu_id, i = item)
+    #return "This page is for editing menu item %s" % menu_id
 
 
-@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete/')
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete/', methods=['GET', 'POST'])
 def deleteMenuItem(restaurant_id, menu_id):
-    return "This page is for deleting menu item %s" % menu_id
+    return render_template('deleteMenuItem.html', restaurant_id = restaurant_id, menu_id = menu_id, item = item)
+    #return "This page is for deleting menu item %s" % menu_id
 
+
+
+#Fake Restaurants for testing templates
+restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
+
+restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers',
+                'id':'2'},{'name':'Taco Hut', 'id':'3'}]
+
+#Fake Menu Items
+items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'},
+          {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},
+          {'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},
+          {'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},
+          {'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
+item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree'}
 
 
 if __name__ == '__main__':
